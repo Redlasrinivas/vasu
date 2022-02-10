cf7c81ccd7bca8bf19bcab5f78967396a5ec6e57
Redlasrinivas <99330721+Redlasrinivas@users.noreply.github.com>
2022-02-10 20:13:40 +0530

    Create 2nd.py

---
+++ 2nd.py
@@ -0,0 +1,3 @@
+#PROGRAM FOR STUDENT RESULTSStu_no=int(input("Enter the student no : "))Stu_name=input("Enter the student name : "))Stu_sec=input("Enter the student group : "))Sub1=int(input("Enter the computers(1) marks : "))Sub2=int(input("Enter the computers(2) marks : "))Sub3=int(input("Enter the computers(3) marks : "))if Sub1>=35 and Sub2>=35 and Sub3>=35: print(Stu_name,"-----PASS.")else: print(Stu_nmae,"-----FAIL.")
+
+
